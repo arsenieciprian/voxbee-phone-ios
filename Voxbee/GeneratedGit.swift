37254bd4bb8d80f99ef307c6839fec38b0672b29
Ciprian Arsenie <arsenieciprian@gmail.com>
2026-02-28 13:52:37 +0200

    fix: remove invalid export compliance code and bump build number

--- Voxbee/GeneratedGit.swift
+++ Voxbee/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "voxbee-ios"
-    public static let commit = "ed0e177ac"
+    public static let commit = "3d13a86be"
     public static let tag = "6.1.0-alpha"
 }
