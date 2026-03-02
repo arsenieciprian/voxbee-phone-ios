ee9871db796493a20c25439c47bdc3c0ba7247a2
Ciprian Arsenie <arsenieciprian@gmail.com>
2026-03-02 17:58:25 +0200

    Clean up AccountManager URL logic and finalize iOS provisioning settings

--- Voxbee/GeneratedGit.swift
+++ Voxbee/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "main"
-    public static let commit = "32eeb57d"
+    public static let commit = "231215f0"
     public static let tag = "no-tag"
 }
